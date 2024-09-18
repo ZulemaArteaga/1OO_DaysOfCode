6243f5b6045de485590c6eda374be210e2d21037
Zulema Arteaga <zulema.arteaga@gmail.com>
2024-09-18 14:31:00 -0400

    Adding task. Day 5.

---
+++ Day_5/Highest Score/task.py
@@ -0,0 +1,28 @@
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+print(range(1, 10))
+
+# Sum Scores with "sum" built-in function
+total_exam_score = sum(student_scores)
+print(total_exam_score)
+# Sum Scores with loop
+sum = 0
+for score in student_scores:
+    sum += score
+print(sum)
+
+# Max Score with "max" built-in function
+print(max(student_scores))
+# Max Score with loop
+max_score = None
+for score in student_scores:
+    if max_score is None or score > max_score: max_score = score
+print(max_score)
+
+# Min Score with "min" built-in function
+print(min(student_scores))
+# Max Score with loop
+min_score = None
+for score in student_scores:
+    if min_score is None or score < min_score: min_score = score
+print(min_score)
+
---
+++ Day_5/Password Generator Project/task.py
@@ -0,0 +1,53 @@
+'''The objective is to take the inputs from the user to these questions and then generate a random password. '''
+
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+while True:
+    # Prompts the user for integer input, validates it, and repeats if invalid.
+    try:
+        nr_letters = int(input("How many letters would you like in your password?\n"))
+        nr_symbols = int(input(f"How many symbols would you like?\n"))
+        nr_numbers = int(input(f"How many numbers would you like?\n"))
+        break
+    except ValueError:
+        print("INVALID OPTION. Please select a numeric value")
+
+
+# EASY VERSION. Password with Pattern: First Letters then Symbols and last Numbers
+password = []
+for letter in range(nr_letters):
+    random_letters = random.choice(letters)
+    password.append(random_letters)
+
+for symbol in range(nr_symbols):
+    random_symbols = random.choice(symbols)
+    password.append(random_symbols)
+
+for number in range(nr_numbers):
+    random_number = random.choice(numbers)
+    password.append(random_number)
+
+print("Your Password is: " + ''.join(password))
+print("This password follow a sequence: letter > symbols > numbers\n")
+
+
+# HARD VERSION. Password does not follow a pattern
+password = []
+for each in range(nr_letters) and range(nr_symbols) and range(nr_numbers):
+    random_letters = random.choice(letters)
+    random_symbols = random.choice(symbols)
+    random_number = random.choice(numbers)
+    password.append(random_letters)
+    password.append(random_symbols)
+    password.append(random_number)
+
+    random.shuffle(password)
+
+print("Your Password is: " + ''.join(password))
+print("This password doesn't follow a pattern")
+
