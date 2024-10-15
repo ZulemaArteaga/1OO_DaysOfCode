244d203d13b2c05fed8e9e69773c18bbb7ecf8f2
Zulema Arteaga <zulema.arteaga@gmail.com>
2024-10-15 16:11:30 -0400

    Deleted the strings TRUE and Love since they are not necessary

--- Day_8/Love_Calculator/task.py
+++ Day_8/Love_Calculator/task.py
@@ -1,9 +1,9 @@
-'''Love Calculator
+"""Love Calculator
 You are going to write a function called calculate_love_score() 
 that tests the compatibility between two names.  To work out the love score between two people: 
 1. Take both people's names and check for the number of times the letters in the word TRUE occurs.   
 2. Then check for the number of times the letters in the word LOVE occurs.   
-3. Then combine these numbers to make a 2 digit number and print it out. 
+3. Then combine these numbers to make a 2-digit number and print it out.
 e.g.
 name1 = "Angela Yu" name2 = "Jack Bauer"
 T occurs 0 times 
@@ -17,17 +17,14 @@ O occurs 0 times
 V occurs 0 times 
 E occurs 2 times 
 Total = 3 
-Love Score = 53'''
+Love Score = 53"""
 
 def calculate_love_score(name1, name2):
-    true = "TRUE"
-    love = "LOVE"
-
     names = name1.upper() + name2.upper()
     names = {char: names.count(char) for char in set(names)} # Counting each letter in names
 
     true_total_sum = 0
-    for key in true:
+    for key in "TRUE":
         if key in names:
             print(f"{key} occurs: {names[key]} times")
             true_total_sum += names[key]
@@ -35,7 +32,7 @@ def calculate_love_score(name1, name2):
     print(f"Total = {true_total_sum}")
 
     love_total_sum = 0
-    for key in love:
+    for key in "LOVE":
         if key in names:
             print(f"{key} occurs: {names[key]} times")
             love_total_sum += names[key]
