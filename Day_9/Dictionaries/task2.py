f8239c8bb638d0552b699591b8e25042eeeb4d1e
Zulema Arteaga <zulema.arteaga@gmail.com>
2025-04-03 17:31:12 -0400

    Corrected the scoring criteria for the 71-80 range

--- Day_9/Dictionaries/task2.py
+++ Day_9/Dictionaries/task2.py
@@ -28,7 +28,7 @@ student_grades = {}
 for student in student_scores:
     if student_scores[student] in range(91,101): student_grades[student] = "Outstanding"
     elif student_scores[student] in range(81,90): student_grades[student] = "Exceeds Expectations"
-    elif student_scores[student] in range(71, 80): student_grades[student] = "Exceeds Expectations"
+    elif student_scores[student] in range(71, 80): student_grades[student] = "Acceptable"
     elif student_scores[student] in range(0,70): student_grades[student] = "Fail"
 print(student_grades)
 
